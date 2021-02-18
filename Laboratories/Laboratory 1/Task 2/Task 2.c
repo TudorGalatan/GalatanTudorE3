4d36d3aa7f2716e0499da3af32c9e86f35c54a90
Tudor Gălățan <tudorgalatan@gmail.com>
2021-02-18 22:58:45 +0200

    Improved Laboratory 1 - Task 2
    I've improved the task 2 for the laboratory 1. I've also added the task statements.

--- Laboratories/Laboratory 1/Task 2/Task 2.c
+++ Laboratories/Laboratory 1/Task 2/Task 2.c
@@ -4,51 +4,81 @@
 #include <string.h>
 
 
-int stringToNumber (char string[])
+void calculateSum ();
+long long int stringToNumber (char string[]);
+
+
+int main ()
 {
-	int number = 0;
+	// Read the input data, calculate the requested sum
+	// and print it on the screen.
+	calculateSum();
 
-	unsigned short int negative = 0;
+	return 0;
+}
 
-	if (string[0] == '-')
-		negative = 1;
-	else
+
+/*
+	Reads the input data, calculates the requested sum and
+	prints it on the screen.
+	Input:
+		- none
+	Output:
+		- the requested sum is printed on the screen
+*/
+void calculateSum ()
+{
+	// Open the input file.
+	FILE* inputFile = fopen("in.txt", "r");
+
+	long long int sum = 0;
+	char string[10000];
+
+	// For every line, convert the string into a number and
+	// build the sum.
+	while (fgets(string, 10000, inputFile))
 	{
-		unsigned short int digit = string[0] - '0';
-		number = number * 10 + digit;
+		// The last character on every line, except the last one,
+		// is a line break, which cannot be part of the number.
+		if (string[strlen(string) - 1] == '\n')
+			string[strlen(string) - 1] = '\0';
+
+		// Convert the string into a number.
+		long long int number = stringToNumber(string);
+
+		// Build the requested sum.
+		sum += number;
 	}
 
-	for (unsigned int index = 1; index < strlen(string); index++)
+	// Print the requested sum on the screen.
+	printf("%lld\n", sum);
+}
+
+
+/*
+	Converts a string into a number.
+	Input:
+		- "string": the given string
+	Output:
+		- the number corresponding to the string's content
+*/
+long long int stringToNumber (char string[])
+{
+	// If the first character of the string is '-', then we have a negative number.
+	unsigned short int isNegative = 0;
+	if (string[0] == '-')
+		isNegative = 1;
+
+	// Take every character, convert it to a digit and build the number digit by digit.
+	long long int number = 0;
+	for (unsigned int index = isNegative; index < strlen(string); index++)
 	{
 		unsigned short int digit = string[index] - '0';
 		number = number * 10 + digit;
 	}
 
-	if (negative)
-		number -= 2 * number;
+	if (isNegative)
+		number = -number;
 
 	return number;
-}
-
-
-int main ()
-{
-	FILE* inputFile = fopen("in.txt", "r");
-
-	unsigned int number = 0;
-	long long int sum = 0;
-
-	char word[100];
-
-	while (fgets(word, 100, inputFile))
-	{
-		if (word[strlen(word) - 1] == '\n')
-			word[strlen(word) - 1] = '\0';
-		int number = stringToNumber(word);
-		sum += number;
-	}
-
-	printf("%lld\n", sum);
-
-	return 0;
 }
