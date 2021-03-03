0ba7c26ffb77e125db0a5ba098168111615f8cb1
Tudor Gălățan <tudorgalatan@gmail.com>
2021-03-03 23:39:11 +0200

    Improved Laboratory 3 - Task 1
    I've improved the first task from the third laboratory.

--- Laboratories/Laboratory 3/Task 1/Solution/Mathematics.cpp
+++ Laboratories/Laboratory 3/Task 1/Solution/Mathematics.cpp
@@ -5,61 +5,77 @@
 #include "Mathematics.h"
 
 
-int Mathematics::addNumbers (int firstNumber, int secondNumber)
+int Mathematics::add (int firstNumber, int secondNumber)
 {
 	return firstNumber + secondNumber;
 }
 
 
-int Mathematics::addNumbers (int firstNumber, int secondNumber, int thirdNumber)
+int Mathematics::add (int firstNumber, int secondNumber, int thirdNumber)
 {
 	return firstNumber + secondNumber + thirdNumber;
 }
 
 
-double Mathematics::addNumbers (double firstNumber, double secondNumber)
+double Mathematics::add (double firstNumber, double secondNumber)
 {
 	return firstNumber + secondNumber;
 }
 
 
-double Mathematics::addNumbers (double firstNumber, double secondNumber, double thirdNumber)
+double Mathematics::add (double firstNumber, double secondNumber, double thirdNumber)
 {
 	return firstNumber + secondNumber + thirdNumber;
 }
 
 
-int Mathematics::addNumbers (int count, ...)
+int Mathematics::add (unsigned int numberOfNumbers, ...)
 {
-	return 0;
+	va_list numbers;
+
+	va_start(numbers, numberOfNumbers);
+
+	int sum = 0;
+	for (unsigned int index = 0; index < numberOfNumbers; index++)
+		sum += va_arg(numbers, int);
+
+	va_end(numbers);
+
+	return sum;
 }
 
 
-char* Mathematics::addNumbers (const char*, const char*)
+char* Mathematics::add (const char* firstString, const char* secondString)
 {
-	return 0;
+	if (firstString == nullptr or secondString == nullptr)
+		return nullptr;
+
+	char* finalString = (char*)firstString;
+	strcat(finalString, secondString);
+
+	return finalString;
 }
 
 
-int Mathematics::multiplyNumbers (int firstNumber, int secondNumber)
+int Mathematics::multiply (int firstNumber, int secondNumber)
 {
 	return firstNumber * secondNumber;
 }
 
 
-int Mathematics::multiplyNumbers (int firstNumber, int secondNumber, int thirdNumber)
+int Mathematics::multiply (int firstNumber, int secondNumber, int thirdNumber)
 {
 	return firstNumber * secondNumber * thirdNumber;
 }
 
 
-double Mathematics::multiplyNumbers (double firstNumber, double secondNumber)
+double Mathematics::multiply (double firstNumber, double secondNumber)
 {
 	return firstNumber * secondNumber;
 }
 
 
-double Mathematics::multiplyNumbers (double firstNumber, double secondNumber, double thirdNumber)
+double Mathematics::multiply (double firstNumber, double secondNumber, double thirdNumber)
 {
 	return firstNumber * secondNumber * thirdNumber;
 }
--- Laboratories/Laboratory 3/Task 1/Solution/Mathematics.h
+++ Laboratories/Laboratory 3/Task 1/Solution/Mathematics.h
@@ -2,21 +2,116 @@
 	This is the interface for the Mathematics module.
 */
 
+#define _CRT_SECURE_NO_WARNINGS
 #pragma once
+
+#include <stdarg.h>
+#include <string.h>
 
 
 class Mathematics
 {
     public:
 
-        static int addNumbers (int, int);
-        static int addNumbers (int, int, int);
-        static double addNumbers (double, double);
-        static double addNumbers (double, double, double);
-        static int addNumbers (int count, ...);
-        static char* addNumbers (const char*, const char*);
-        static int multiplyNumbers (int, int);
-        static int multiplyNumbers (int, int, int);
-        static double multiplyNumbers (double, double);
-        static double multiplyNumbers (double, double, double);
+        /*
+            Adds two numbers of type integer.
+            Input:
+                - two integer numbers
+            Output:
+                - an integer value representing the sum of the
+                  two numbers
+        */
+        static int add (int, int);
+
+        /*
+            Adds three numbers of type integer.
+            Input:
+                - three integer numbers
+            Output:
+                - an integer value representing the sum of the
+                  three numbers
+        */
+        static int add (int, int, int);
+
+        /*
+            Adds two numbers of type double.
+            Input:
+                - two double numbers
+            Output:
+                - a double value representing the sum of the
+                  two numbers
+        */
+        static double add (double, double);
+
+        /*
+            Adds three numbers of type double.
+            Input:
+                - three double numbers
+            Output:
+                - a double value representing the sum of the
+                  three numbers
+        */
+        static double add (double, double, double);
+
+        /*
+            Adds up a list of integer numbers.
+            Input:
+                - an unsigned integer number representing the
+                  number of numbers in the list
+                - a list of integer numbers
+            Output:
+                - an integer value representing the sum of the
+                  list of numbers
+        */
+        static int add (unsigned int, ...);
+
+        /*
+            Concatenates two strings together.
+            Input:
+                - two strings
+            Output:
+                - nullptr, if any of the two strings is nullptr
+                - the concatenation of the two strings, otherwise
+        */
+        static char* add (const char*, const char*);
+
+        /*
+            Multiplies two numbers of type integer.
+            Input:
+                - two integer numbers
+            Output:
+                - an integer value representing the multiplication
+                  of the two numbers
+        */
+        static int multiply (int, int);
+
+        /*
+            Multiplies three numbers of type integer.
+            Input:
+                - three integer numbers
+            Output:
+                - an integer value representing the multiplication
+                  of the three numbers
+        */
+        static int multiply (int, int, int);
+
+        /*
+            Multiplies two numbers of type double.
+            Input:
+                - two double numbers
+            Output:
+                - a double value representing the multiplication
+                  of the two numbers
+        */
+        static double multiply (double, double);
+
+        /*
+            Multiplies three numbers of type double.
+            Input:
+                - three double numbers
+            Output:
+                - a double value representing the multiplication
+                  of the three numbers
+        */
+        static double multiply (double, double, double);
 };
