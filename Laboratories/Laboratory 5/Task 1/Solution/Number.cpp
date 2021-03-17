26993c6e4979037667c38a89f3a92ad7bb2d2b19
Tudor Gălățan <tudorgalatan@gmail.com>
2021-03-18 01:51:53 +0200

    Improved Laboratory 5 - Task 1
    I've improved the first task for the fifth laboratory.

--- Laboratories/Laboratory 5/Task 1/Solution/Number.cpp
+++ Laboratories/Laboratory 5/Task 1/Solution/Number.cpp
@@ -5,39 +5,129 @@
 #include "Number.h"
 
 
-Number::Number (const char* value, unsigned short int base)
+Number::Number (const char* digits, unsigned short int base)
 {
-	// To add.
+	// Get the size of the "digits" string.
+	unsigned short int stringSize = strlen(digits) + 1;
+
+	// Allocate memory for the current instance of the class.
+	this->digits = new char[stringSize];
+	this->base = new unsigned short int;
+
+	// Assign the given data to the current instance of the class.
+	memcpy(this->digits, digits, stringSize);
+	*this->base = base;
+}
+
+
+Number::Number (const Number& number)
+{
+	this->digits = number.digits;
+	*this->base = *number.base;
+}
+
+
+Number::Number (Number&& number) noexcept
+{
+	// Move the digits of the number.
+	char* temporaryString = number.digits;
+	number.digits = nullptr;
+	this->digits = temporaryString;
+
+	// Move the base of the number.
+	unsigned short int* base = number.base;
+	delete number.base;
+	this->base = base;
 }
 
 
 Number::~Number ()
 {
-	// To add.
+	// Deallocate the memory used for storing the digits of the number.
+	delete this->digits;
+	this->digits = nullptr;
+
+	// Deallocate the memory used for storing the base of the number.
+	delete this->base;
+}
+
+
+char* Number::operator + (const Number& number)
+{
+	unsigned short int resultSize = strlen(number.digits) + strlen(this->digits) + 3;
+	char* result = new char[resultSize];
+
+	// Perform the addition of the two numbers.
+	return NULL;
+}
+
+
+char* Number::operator - (const Number& number)
+{
+	return NULL;
+}
+
+
+char* Number::operator ! ()
+{
+	return NULL;
+}
+
+
+Number Number::operator < (const Number& number)
+{
+	return number;
+}
+
+
+Number Number::operator <= (const Number& number)
+{
+	return number;
+}
+
+
+Number Number::operator == (const Number& number)
+{
+	return number;
+}
+
+
+Number Number::operator >= (const Number& number)
+{
+	return number;
+}
+
+
+Number Number::operator > (const Number& number)
+{
+	return number;
+}
+
+
+void Number::print ()
+{
+	std::cout << "\n\tThe number under base " << *this->base << " is: ";
+
+	unsigned short int numberOfDigits = this->getNumberOfDigits();
+	for (unsigned short int index = 0; index < numberOfDigits; index++)
+		std::cout << this->digits[index];
+	std::cout << ".\n";
+}
+
+
+unsigned short int Number::getNumberOfDigits ()
+{
+	return strlen(this->digits);
+}
+
+
+unsigned short int Number::getBase ()
+{
+	return *this->base;
 }
 
 
 void Number::changeBase (unsigned short int newBase)
 {
 	// To add.
-}
-
-
-void Number::print ()
-{
-	// To add.
-}
-
-
-int Number::getNumberOfDigits ()
-{
-	// To add.
-	return 0;
-}
-
-
-int Number::getBase ()
-{
-	// To add.
-	return 0;
 }
--- Laboratories/Laboratory 5/Task 1/Solution/Number.h
+++ Laboratories/Laboratory 5/Task 1/Solution/Number.h
@@ -4,22 +4,102 @@
 
 #pragma once
 
-#include <string>
+#include <cstring>
+#include <iostream>
 
 
 class Number
 {
 	private:
 
-		std::string digits;
-		unsigned short int base;
+		char* digits;
+		unsigned short int* base;
 
 	public:
 
-		Number(const char* value, unsigned short int base);
-		~Number();
-		void changeBase(unsigned short int newBase);
-		void print();
-		int getNumberOfDigits();
-		int getBase();
+		/*
+			This is the main constructor of the class. It initializes the data with
+			the given values.
+			Input:
+				- "digits": the digits of the number
+				- "base": the base of the number
+			Output:
+				- the data is being initialized with the given values
+		*/
+		Number (const char* digits, unsigned short int base);
+
+		/*
+			This is the copy constructor of the class. It initializes the data with
+			the values of the other class instance.
+			Input:
+				- "number": the other class instance
+			Output:
+				- the data is being initialized with the values of the other class
+				  instance
+		*/
+		Number (const Number& number);
+
+		/*
+			This is the move constructor of the class.
+			Input:
+				- "number": the other class instance
+			Output:
+				- the data is being initialized
+		*/
+		Number (Number&& number) noexcept;
+
+		/*
+			This is the destructor of the class. It destroys the current instance of
+			the class.
+			Input:
+				- none
+			Output:
+				- the current instance of the class is being destroyed
+		*/
+		~Number ();
+
+		char* operator + (const Number& number);
+		char* operator - (const Number& number);
+		char* operator ! ();
+		Number operator < (const Number& number);
+		Number operator <= (const Number& number);
+		Number operator == (const Number& number);
+		Number operator >= (const Number& number);
+		Number operator > (const Number& number);
+
+		/*
+			Prints the number on the screen.
+			Input:
+				- none
+			Output:
+				- the number is being printed on the screen
+		*/
+		void print ();
+
+		/*
+			Gets the number of digits of the number.
+			Input:
+				- none
+			Output:
+				- the number of digits of the number
+		*/
+		unsigned short int getNumberOfDigits ();
+
+		/*
+			Gets the base of the number.
+			Input:
+				- none
+			Output:
+				- the base of the number
+		*/
+		unsigned short int getBase ();
+
+		/*
+			Changes the base of the number.
+			Input:
+				- "newBase": the new base of the number
+			Output:
+				- the number is converted to the new base
+		*/
+		void changeBase (unsigned short int newBase);
 };
